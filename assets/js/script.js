dde7d50430012e01c2962bb0dd28438758e9d5fd
Sourav Mondal <108756502+Sourav-gittech@users.noreply.github.com>
2025-02-23 11:34:09 +0530

    commited

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,5 +41,28 @@ navLinks.forEach((a) => {
     a.addEventListener('click', () => {
         navBar.classList.remove('active');
         openORcloseMenu("1","auto","auto","auto");
-    })
-})
+    });
+});
+
+// counter design 
+document.addEventListener('DOMContentLoaded', () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter('count', 0, 10, 6000);
+    counter('count1', 0, 90, 6000);
+    counter('count2', 0, 85, 6000);
+    counter('count3', 0, 95, 6000);
+    counter('count4', 0, 100, 6000);
+});
